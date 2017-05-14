a664440b2e9987b114adb21522b234cfe228c7c6
RichardAlcalaC <jheranium@gmail.com>
2017-05-14 06:42:33 -0400

    Initial sharing of project

---
+++ TestMain.java
@@ -0,0 +1,61 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class TestMain.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class TestMain
+{
+    /**
+     * Default constructor for test class TestMain
+     */
+    public TestMain()
+    {
+    }
+    
+    @Test
+    public void numberOfSingleDigitIsCapicua()
+    {
+        assertTrue(Main.numberIsCapicua(3));
+    }
+    
+    @Test
+    public void numberWithAllEqualDigitsIsCapicua()
+    {
+        assertTrue(Main.numberIsCapicua(22));
+        assertTrue(Main.numberIsCapicua(333));
+        assertTrue(Main.numberIsCapicua(444));
+
+    }
+    
+    @Test
+    public void hundredNumberIsNotCapicua()
+    {
+        assertFalse(Main.numberIsCapicua(100));
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
